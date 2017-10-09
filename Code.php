9c477530ea4ee55e4d323af99f748f4e870e4fbb
Msknndre <32644563+Msknndre@users.noreply.github.com>
2017-10-09 10:20:37 -0500

    Add files via upload

---
+++ Code.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+	 <?php
+		$variable = 0
+		if (variable == 27)
+			echo "How did this happen?";
+		else
+			echo "It worked!";
+	 
+	 ?>
+ </body>
+</html>
